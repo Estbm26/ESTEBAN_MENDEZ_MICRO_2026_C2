b253bbe51c75758d3c1a06d2304bb2a3bfcc27c4
Esteban <estmendezabreu@gmail.com>
2026-07-01 22:34:40 -0400

    Encender led mqtt

---
+++ 3ra tarea/mqtt5/primer project/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float x = 0.0;
+    float *px;
+
+    px = &x;
+
+    printf("\n Direccion de X: %p", px);
+    printf("\n valor de x: %f" *px);
+    printf("\n valor de x: ")
+
+    return 0;
+}
